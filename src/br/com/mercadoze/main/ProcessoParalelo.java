1a545e266c88993b63682128ecf78d9ea1fd5aa9
Fernando Costa <fernandolocos@gmail.com>
2015-07-23 21:32:42 -0300

    Aula do dia 23 JUL 15 - versão 1

---
+++ src/br/com/mercadoze/main/ProcessoParalelo.java
@@ -0,0 +1,32 @@
+package br.com.mercadoze.main;
+
+public class ProcessoParalelo implements Runnable {
+
+	private String nome;
+	
+	public ProcessoParalelo(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public void run() {
+		while(true){
+			System.out.println(this.nome);
+			try {
+				Thread.currentThread().sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
